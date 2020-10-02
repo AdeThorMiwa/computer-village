72198f2006611ee7a0f0a692f167b4659d7bed0a
AdeThorMiwa <bendamyth@gmail.com>
2020-10-02 00:06:38 -1100

    updating faqs

--- src/components/FAQTable/FAQTable.js
+++ src/components/FAQTable/FAQTable.js
@@ -4,32 +4,32 @@ import { FAQTableContainer } from "./style";
 const FAQTable = () => {
   const data = [
     {
-      title: "SHIPPING",
+      title: "SELLER",
       faqs: [
         {
-          question: "What Shipping Methods Are Available",
+          question: "How do i become a seller",
           answer:
-            "All you gotta do is pay the  cash and your good will be delivered to you just in time",
+            "Simply click on the register link and register with us. After registration you automatically get a store created for you.",
         },
         {
-          question: "How do i get My Good",
+          question: "How do i Upload my Products",
           answer:
-            "All you gotta do is pay the  cash and your good will be delivered to you just in time",
+            "Login to your dashboard and simply go to the products page. Click the add product button and fill the next form accordingly.",
         },
       ],
     },
     {
-      title: "PAYMENT",
+      title: "BUYERS",
       faqs: [
         {
-          question: "What Payent Methods Are Available",
+          question: "How do i get a product i like.",
           answer:
-            "All you gotta do is pay the  cash and your good will be delivered to you just in time",
+            "Simply click on the product to view it. Once you're taken to the product page, you see the store contact detail at the top of the page. Alternatively, just click on the view store direct to see view the store and ultimately get the store contact detail.",
         },
         {
-          question: "How do i get My Paymetn",
+          question: "How do i make payment",
           answer:
-            "All you gotta do is pay the  cash and your good will be delivered to you just in time",
+            "Payment and every other seller-buyer interaction has been taken away from the platform. But you can get a seller contact detail from viewing their store, thereby having more control on how your goods are delivered.",
         },
       ],
     },
